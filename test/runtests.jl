098e45d36c3d8ef327f7ac801ef81925eb1aa28e
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-10-12 11:12:36 +0200

    Test of basis evaluation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using BSplines
 using Test
+using LinearAlgebra
+using SparseArrays
+
+function vecdist(a::AbstractVector, b::AbstractVector,
+                 ϵ = eps(eltype(a)))
+    δ = √(sum(abs2, a-b))
+    δ, δ/√(sum(abs2, a .+ ϵ))
+end
 
 @testset "Knot sets" begin
     t = LinearKnotSet(7, 0, 1, 10)
@@ -31,4 +39,33 @@ end
     x,w = BSplines.lgwt(t)
     @test all(w .== 1/4)
     @test x == [-1,1,-1,1]/(4*√3) + [1,1,3,3]/4
+end
+
+@testset "Basis" begin
+    t = LinearKnotSet(3, 0, 1, 2)
+    basis = BSplines.Basis(t)
+    @testset "Eval on subintervals" begin
+        x₁ = range(-1,stop=-0.5,length=10)
+        Bᵢ₁ = basis(x₁)
+        @test norm(Bᵢ₁) == 0
+
+        function testbasis(x)
+            Bᵢ = basis(x)
+            B̃ = spzeros(Float64, length(x), 4)
+            B̃[:,1] = (x .>= 0) .* (x .< 0.5) .* ((2x .- 1).^2)
+            B̃[:,2] = (x .>= 0) .* (x .< 0.5) .* (2/3*(1 .- (3x .- 1).^2)) +
+                (x .>= 0.5) .* (x .< 1)  .* (2*(x .- 1).^2)
+            B̃[:,3] = (x .>= 0) .* (x .< 0.5) .* (2*x.^2) +
+                (x .>= 0.5) .* (x .< 1)  .* (2/3*(1 .- (3x .- 2).^2))
+            B̃[:,4] = (x .>= 0.5) .* (x .< 1) .* ((2x .- 1).^2)
+            for j = 1:4
+                δ,δr = vecdist(Bᵢ[end][:,j],B̃[:,j])
+                @test δ < 1e-15
+                @test δr < 1e-15
+            end
+        end
+        testbasis(range(0,stop=1,length=50))
+        testbasis(range(-0.5,stop=0.6,length=40))
+        testbasis(range(0.5,stop=1.6,length=40))
+    end
 end
